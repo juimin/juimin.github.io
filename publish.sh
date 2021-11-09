8b90e18f3717b41232dfd5f523a72dc3432693a4
juimin <d95wang@gmail.com>
2021-11-08 20:41:55 -0800

    progress publish

--- publish.sh
+++ publish.sh
@@ -3,8 +3,24 @@
 # Run the build
 npm run build
 
+cp -r ./dist /tmp/.personalsite
+
+# Switch to hosting branch
 git checkout -B gh-pages
+git branch --set-upstream-to=origin/gh-pages
+git pull
+
 # Remove everything
+rm -rf ./* ./.*
+
+cp /tmp/.personalsite/* ./
+
+git add .
+git commit -m "publishing command"
+git status
 
 # Switch back to the master branch
 # git checkout -B master
+
+# Clean up
+rm -r /tmp/.personalsite
