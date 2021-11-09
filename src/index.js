d06066da6320de61a98572439d3c485cfc843f0a
juimin <d95wang@gmail.com>
2021-11-08 21:42:34 -0800

    add homepage and portfolio stubs

--- src/index.js
+++ src/index.js
@@ -1,6 +1,27 @@
 import ReactDOM from "react-dom";
 import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import Homepage from "./homepage";
+import Portfolio from "./portfolio";
+
 const App = () => {
-    return <h1>This is my React app</h1>;
+    return <div>
+        <h1>Derek Wang</h1>
+        <div>Placeholder for fun graphics</div>
+        <Routes>
+            <Route path="/" element={<Homepage />} />
+            <Route path="portfolio" element={<Portfolio />} />
+        </Routes>
+    </div>
 }
-ReactDOM.render(<App />, document.getElementById("app"));
+
+
+// Function returning the browser router wrapped application component
+const RouterWrappedApp = () => {
+    return <BrowserRouter>
+        <App />
+    </BrowserRouter>
+}
+
+ReactDOM.render(RouterWrappedApp(), document.getElementById("app"));
